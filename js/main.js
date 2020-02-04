91ea96b131f396e9827c4f03079f3a1c34e9ef2c
Kristina Kuzenko <k.kuzenko@gmail.com>
2020-02-05 00:56:17 +0200

    js add banana

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$("div.container").hide();
+$("#add").click(function() {
+    addItem("banana");
+});
+var LIST = $('#products-list');
+var ITEM_TEMPLATE = $('#line').html();
+
+function addItem(title) {
+    var node = $(ITEM_TEMPLATE); //Create	new	HTML	node
+    node.find(".product").text(title); //Set	product	title
+    //Delete	Action
+    node.find("#delete-button").click(function() {
+        node.remove();
+    });
+    LIST.append(node); //Add	to	the	end	of	the	list
+}
