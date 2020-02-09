ec3531e3c373a00343e88f79799936f01a29baf1
Kristina Kuzenko <k.kuzenko@gmail.com>
2020-02-09 16:47:14 +0200

    Plus,minus

--- js/main.js
+++ js/main.js
@@ -10,26 +10,45 @@ function addItem(title) {
     var t = $(ITEM_TEMPLATEmini); //Create	new	HTML	node
     t.find("#name").text(title); //Set	product	title
 
+
     LISTmini.append(t); //Add	to	the	end	of	the	list
     var node = $(ITEM_TEMPLATE); //Create	new	HTML	node
+    var k = parseInt(node.find("#qty").text(), 10);
     node.find(".product").text(title);
     t.find("#name").text(title); //Set	product	title//Set	product	title
     node.find("#delete").click(function() {
         node.remove();
         t.remove();
     });
+    node.find("#plus").click(function() {
+        var n = parseInt(node.find("#qty").text(), 10);
+        node.find("#minus").prop('disabled', false);
+        node.find("#qty").text(n + 1);
+        t.find("#qty").text(n + 1);
+    });
+    node.find("#minus").click(function() {
+        var n = parseInt(node.find("#qty").text(), 10);
+        if (n > 1) {
+            node.find("#minus").prop('disabled', false);
+            node.find("#qty").text(n - 1);
+            t.find("#qty").text(n - 1);
+        } else {
+            node.find("#minus").prop('disabled', true);
+        }
+    });
+    node.find("#bought").click(function() {
+        var one = $("#buym");
+        var two = $("#buyp").html();
+
+        var no = $(two); //Create	new	HTML	node
+        no.find("#name").text(title); //Set	product	title
+        one.append(no);
+        t.remove();
+    });
     LIST.append(node); //Add	to	the	end	of	the	list
 }
 
 
-var one = $("#buym");
-var two = $("#buyp").html();
-
-function move() {
-    var no = $(two); //Create	new	HTML	node
-    no.find("#name").text("blabla"); //Set	product	title
-    one.append(no);
-}
 
 $("#add").click(function() {
     var inputVal = document.getElementById("my-input").value;
@@ -44,8 +63,4 @@ $(document).keyup(function(event) {
         addItem(inputVal);
 
     }
-});
-
-$("#bought").click(function() {
-    move();
 });
