a7908406fe4b234685218ef193a29778368ec204
Kristina Kuzenko <k.kuzenko@gmail.com>
2020-02-10 23:04:31 +0200

    All buttons

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@ var LIST = $("#products-list");
 var ITEM_TEMPLATE = $("#line").html();
 var LISTmini = $("#mini");
 var ITEM_TEMPLATEmini = $('#pr').html();
+var one = $("#buym");
+var two = $("#buyp").html();
 addItem("Banana");
 addItem("Pineapple");
 addItem("Avocado");
 
 function addItem(title) {
-    var t = $(ITEM_TEMPLATEmini); //Create	new	HTML	node
-    t.find("#name").text(title); //Set	product	title
-
-
-    LISTmini.append(t); //Add	to	the	end	of	the	list
-    var node = $(ITEM_TEMPLATE); //Create	new	HTML	node
-    var k = parseInt(node.find("#qty").text(), 10);
+    var no = $(two);
+    var t = $(ITEM_TEMPLATEmini);
+    t.find("#name").text(title);
+    LISTmini.append(t);
+    var node = $(ITEM_TEMPLATE);
+    node.find("#minus").prop('disabled', true);
+    node.find("#notb").css({ display: "none" });
+    node.find("#buyy").css({ display: "flex" });
     node.find(".product").text(title);
-    t.find("#name").text(title); //Set	product	title//Set	product	title
     node.find("#delete").click(function() {
         node.remove();
         t.remove();
@@ -24,6 +26,7 @@ function addItem(title) {
         var n = parseInt(node.find("#qty").text(), 10);
         node.find("#minus").prop('disabled', false);
         node.find("#qty").text(n + 1);
+        node.find("#qty2").text(n + 1);
         t.find("#qty").text(n + 1);
     });
     node.find("#minus").click(function() {
@@ -31,25 +34,40 @@ function addItem(title) {
         if (n > 1) {
             node.find("#minus").prop('disabled', false);
             node.find("#qty").text(n - 1);
+            node.find("#qty2").text(n - 1);
             t.find("#qty").text(n - 1);
+        } else {
+            node.find("#minus").prop('disabled', true);
+        }
+        var k = parseInt(node.find("#qty").text(), 10);
+        if (k > 1) {
+            node.find("#minus").prop('disabled', false);
+
         } else {
             node.find("#minus").prop('disabled', true);
         }
     });
     node.find("#bought").click(function() {
-        var one = $("#buym");
-        var two = $("#buyp").html();
-
-        var no = $(two); //Create	new	HTML	node
-        no.find("#name").text(title); //Set	product	title
+        var nn = parseInt(node.find("#qty").text(), 10);
+        no.find("#qty").text(nn);
+        no.find("#name").text(title);
         one.append(no);
         t.remove();
+        node.find("#buyy").css({ display: "none" });
+        node.find("#notb").css({ display: "flex" });
+
     });
-    LIST.append(node); //Add	to	the	end	of	the	list
+    node.find("#not-bought").click(function() {
+        t.find("#name").text(title);
+        LISTmini.append(t);
+        no.remove();
+        node.find("#buyy").css({ display: "flex" });
+        node.find("#notb").css({ display: "none" });
+
+    });
+
+    LIST.append(node);
 }
-
-
-
 $("#add").click(function() {
     var inputVal = document.getElementById("my-input").value;
     $("#my-input").val('');
