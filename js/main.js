601d5cc62f305d91ce59d92bcd1063587242651d
Kristina Kuzenko <k.kuzenko@gmail.com>
2020-03-23 16:21:47 +0200

    task 3(added input on product)

--- js/main.js
+++ js/main.js
@@ -16,8 +16,10 @@ function addItem(title) {
     var node = $(ITEM_TEMPLATE);
     node.find("#minus").prop('disabled', true);
     node.find("#notb").css({ display: "none" });
+    node.find("#input_pr").css({ display: "none" });
     node.find("#buyy").css({ display: "flex" });
     node.find(".product").text(title);
+    no.find("#name").text(title);
     node.find("#delete").click(function() {
         node.remove();
         t.remove();
@@ -50,7 +52,6 @@ function addItem(title) {
     node.find("#bought").click(function() {
         var nn = parseInt(node.find("#qty").text(), 10);
         no.find("#qty").text(nn);
-        no.find("#name").text(title);
         one.append(no);
         t.remove();
         node.find("#buyy").css({ display: "none" });
@@ -65,9 +66,24 @@ function addItem(title) {
         node.find("#notb").css({ display: "none" });
 
     });
-
+    node.find("#change").click(function() {
+        node.find("#change").css({ display: "none" });
+        var text = node.find("#change").text();
+        node.find("#input_text").val(text);
+        node.find("#input_pr").css({ display: "flex" });
+        node.find("#input_text").focus();
+    });
+    node.find("#input_pr").mouseout(function(event) {
+        var name = node.find("#input_text").val();
+        t.find("#name").text(name);
+        node.find(".product").text(name);
+        no.find("#name").text(name);
+        node.find("#input_pr").css({ display: "none" });
+        node.find("#change").css({ display: "flex" });
+    });
     LIST.append(node);
 }
+
 $("#add").click(function() {
     var inputVal = document.getElementById("my-input").value;
     $("#my-input").val('');
